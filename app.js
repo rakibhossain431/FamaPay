4d568e3d48a5fdb680499506e0dcb7880f7c0134
rakibhossain431 <rakibjp431@gmail.com>
2022-10-09 03:06:54 +0600

    FamaPay first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+// $('.slick-slider').slick({
+//     dots: true,
+//     infinite: true,
+//     arrows: false,
+//     speed: 400,
+//     slidesToShow: 1,
+//     adaptiveHeight: true,
+//     autoplay: true,
+//     autoplaySpeed: 3000,
+//   });
