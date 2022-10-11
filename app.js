7441e4523161bdb1a6bfe03336b2db200be379b6
Rakib Hossain <rakibjp431@gmail.com>
2022-10-12 00:53:21 +0600

    Add Mobail menu and Slider

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 
 
-// $('.slick-slider').slick({
-//     dots: true,
-//     infinite: true,
-//     arrows: false,
-//     speed: 400,
-//     slidesToShow: 1,
-//     adaptiveHeight: true,
-//     autoplay: true,
-//     autoplaySpeed: 3000,
-//   });
+$('.hero-slider').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+
+const bar =document.querySelector(".sidemenu-btn")
+const menu = document.querySelector(".mobail-menu")
+
+bar.addEventListener("click", ()=>{
+    menu.classList.toggle("show")
+})
+
