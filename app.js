e04bd7b9ef3630ab7f865471ea065588fef82d7a
Rakib Hossain <rakibjp431@gmail.com>
2022-10-14 11:52:34 +0600

    add anather section

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
 
-$('.hero-slider').slick({
-    dots: true,
-    infinite: true,
-    arrows: false,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+// $('.hero-slider').slick({
+//     dots: true,
+//     infinite: true,
+//     arrows: false,
+//     slidesToShow: 1,
+//     adaptiveHeight: true,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//   });
 
 
 const bar =document.querySelector(".sidemenu-btn")
